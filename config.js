6742a14d386d86a6427fbb19a6c49fdc0124f698
Don Pinkus <don.pinkus@gmail.com>
2018-01-08 02:29:45 -0800

    update config

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const config = {};
 
 config.mongoDBURL =
   process.env.NODE_ENV === "production"
-    ? "mongodb://heroku_795jlplm:foobarbaz1@ds245337.mlab.com:45337/heroku_795jlplm"
+    ? "mongodb://heroku_g4dqcfd2:j9kkdnnm2cjgvgslrf90kpofrc@ds245337.mlab.com:45337/heroku_g4dqcfd2"
     : "mongodb://localhost:27017";
 
 config.mongoDBName =
-  process.env.NODE_ENV === "production" ? "heroku_795jlplm" : "poppers_db";
+  process.env.NODE_ENV === "production" ? "heroku_g4dqcfd2" : "poppers_db";
 
 module.exports = config;
