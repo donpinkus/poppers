5c358ad613dc5daf54a6b882b3b8e47b76dd78cc
Don Pinkus <don.pinkus@gmail.com>
2018-01-08 02:18:28 -0800

    add prod mongodb

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 const config = {};
 
 config.mongoDBURL =
-  process.env.NODE_ENV === "production" ? null : "mongodb://localhost:27017";
+  process.env.NODE_ENV === "production"
+    ? "mongodb://<dbuser>:<dbpassword>@ds245337.mlab.com:45337/heroku_795jlplm"
+    : "mongodb://localhost:27017";
 
 config.mongoDBName =
-  process.env.NODE_ENV === "production" ? null : "poppers_db";
+  process.env.NODE_ENV === "production" ? "heroku_795jlplm" : "poppers_db";
 
 module.exports = config;
