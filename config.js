85d4bee2501609725a322e348c664150ab89b56d
Don Pinkus <don.pinkus@gmail.com>
2018-01-07 22:56:22 -0800

    added db

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ config.mongoDBURL =
   process.env.NODE_ENV === "development" ? "mongodb://localhost:27017" : null;
 
 config.mongoDBName =
-  process.env.NODE_ENV === "development" ? "decentralDB" : null;
+  process.env.NODE_ENV === "development" ? "poppersDB" : null;
 
 module.exports = config;
--- data-collectors/pricehistory.js
+++ data-collectors/pricehistory.js
@@ -1,13 +1,20 @@
 var axios = require("axios");
 var tickers = require("./tickers");
-// var tickers = ["BTC", "ETH", "LTC"];
+
+// Stuff for Mongo
+const MongoClient = require("mongodb").MongoClient;
+const mongoDBURL = require("../config").mongoDBURL;
+const mongoDBName = require("../config").mongoDBName;
 
 // Loop through all of our tickers, and hit our API to get their data.
 tickers.forEach(ticker => {
   console.log("Getting data for ticker:", ticker);
 
   // We assume all tickers use "CCCAGG" exchange, whatever the fuck that means
-  var url = "https://min-api.cryptocompare.com/data/histohour?fsym=" + ticker + "&tsym=USD&limit=2000&aggregate=1&e=CCCAGG";
+  var url =
+    "https://min-api.cryptocompare.com/data/histohour?fsym=" +
+    ticker +
+    "&tsym=USD&limit=2000&aggregate=1&e=CCCAGG";
 
   axios.get(url).then(function(response) {
     console.log("Got data for ticker:", ticker);
@@ -17,5 +24,16 @@ tickers.forEach(ticker => {
     var priceData = response.data.Data;
 
     console.log("Price data length:", ticker, priceData.length);
+
+    MongoClient.connect(mongoDBURL, (err, client) => {
+      if (err) throw err;
+
+      const db = client.db(mongoDBName);
+      const collection = db.collection("historicalData");
+      const mongoRecord = {
+        ticker: ticker,
+        data: priceData
+      };
+    });
   });
 });
